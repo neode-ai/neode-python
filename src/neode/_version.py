20a1debc0f9be065a6c77edc81d23ff61e41c2ab
aychang95 <aychang995@gmail.com>
2026-01-28 12:52:21 -0800

    pypi publishing

--- src/neode/_version.py
+++ src/neode/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "neode"
-__version__: str = "0.1.2"
+__version__: str = "0.1.4"
 __openapi_doc_version__: str = "1.0.0"
-__gen_version__: str = "2.801.0"
-__user_agent__: str = "speakeasy-sdk/python 0.1.2 2.801.0 1.0.0 neode"
+__gen_version__: str = "2.801.2"
+__user_agent__: str = "speakeasy-sdk/python 0.1.4 2.801.2 1.0.0 neode"
 
 try:
     if __package__ is not None:
