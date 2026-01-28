36aa04f5de132678fdb3bc5d881b6a599475f919
aychang95 <aychang995@gmail.com>
2026-01-28 03:22:14 -0800

    github integrations

--- src/neode/_version.py
+++ src/neode/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "neode"
-__version__: str = "0.1.1"
+__version__: str = "0.1.2"
 __openapi_doc_version__: str = "1.0.0"
 __gen_version__: str = "2.801.0"
-__user_agent__: str = "speakeasy-sdk/python 0.1.1 2.801.0 1.0.0 neode"
+__user_agent__: str = "speakeasy-sdk/python 0.1.2 2.801.0 1.0.0 neode"
 
 try:
     if __package__ is not None:
